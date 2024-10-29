519aa1a7a4d80fd844ce981b4f18121c043185a8
ApfelTeeSaft <91074565+ApfelTeeSaft@users.noreply.github.com>
2024-10-29 14:46:05 +0100

    Skid of Reboots addresses system, PC is slow so i can't fix the shit because errors don't display properly

---
+++ UniversalSkinChanger/dllmain.cpp
@@ -0,0 +1,31 @@
+// dllmain.cpp : Definiert den Einstiegspunkt für die DLL-Anwendung.
+#include "addresses.h"
+
+
+DWORD WINAPI Main(LPVOID)
+{
+    AllocConsole();
+    FILE* File;
+    freopen_s(&File, "CONOUT$", "w+", stdout);
+    SetConsoleTitleA("UniversalSkinChanger - Dev-v0.0.0.1");
+    SetupVersion();
+    return 0;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        CreateThread(0, 0, Main, 0, 0, 0);
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
---
+++ UniversalSkinChanger/globals.h
@@ -0,0 +1,5 @@
+#pragma once
+
+extern inline int Engine_Version = 0; // For example, 420, 421, etc. // Prevent using this when possible.
+extern inline double Fortnite_Version = 0; // For example, 4.1, 6.21, etc. // Prevent using this when possible.
+extern inline int Fortnite_CL = 0;
